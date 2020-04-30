769531e93d4bea6b60a4a4a5c43befe4b12a6495
Igor Tkachenko <stiles.2126@gmail.com>
2020-04-30 19:39:44 +1000

    Array test passed

--- mapFilterReduce.js
+++ mapFilterReduce.js
@@ -1,4 +1,15 @@
 // put your code in solution constant
-const solution = (array) => array;
+const solution = (array) => {
+    
+    let newArray = array.filter((value) => +value);
+    let newValue = 0;   
+
+    for(let i = 0; i < newArray.length; i++) {
+        newValue += newArray[i]*2;
+    }
+
+    return newValue;
+};
 
 module.exports = solution;
+
