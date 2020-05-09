a79a883bd31a8aa673786321101118c644f936dd
Igor Tkachenko <stiles.2126@gmail.com>
2020-05-09 16:34:15 +1000

    Added function that saves the library in JSON

--- express/storage.js
+++ express/storage.js
@@ -1,7 +1,10 @@
-'use strict'
+// 'use strict'
 
-function storage(){
-  const library = [];
+let fs = require('fs');
+
+
+function storage() {
+  let library = [];
   let id = 0;
 
   return {
@@ -13,46 +16,48 @@ function storage(){
     save: (book) => {
       book.id = id++;
       library.push(book);
+      saveJson(library);
       return book;
     },
 
     find: (id) => {
-      return library.find( (book) => book.id === Number(id) );
-    }, 
+      return library.find((book) => book.id === Number(id));
+    },
 
     update: (id, body) => {
-      let book = library.find( (book) => book.id === Number(id) );
+      let book = library.find((book) => book.id === Number(id));
       book.title = body.title;
       book.author = body.author;
-            
+      saveJson(library);
       return book;
     },
 
     delete: (id) => {
-      let book = library.indexOf( library.find( (book) => book.id === Number(id) ) );
+      let book = library.indexOf(library.find((book) => book.id === Number(id)));
       library.splice(book, 1);
+      saveJson(library);
       return library;
     },
   }
 }
 
 
+function saveJson(obj) {
+  let json = JSON.stringify(obj);
+
+  fs.writeFile('./express/storage.json', json.toString(), (error, data) => {
+    if (error) throw error;
+    console.log("data", data);
+  });
+}
 
 
-// function Counter() {
-//   let count = 0;
-
-//   this.up = function() {
-//     return ++count;
-//   };
-//   this.down = function() {
-//     return --count;
-//   };
+// function getJson() {
+//   let file = fs.readFile('./express/storage.json', "utf8", (error, data) => {
+//     if (error) throw error;
+//     console.log(data);
+//   });
+//   return file;
 // }
 
-// let counter = new Counter();
-
-// alert( counter.up() ); // ?
-// alert( counter.up() ); // ?
-// alert( counter.down() ); // ?
 module.exports = storage;
